d6117c5ba7894a5fa75b8c8d40615394f9680eed
huytv7 <huytv7@msb.com.vn>
2024-09-19 10:18:58 +0700

    init

---
+++ src/main/java/com/example/demospringbootquartzschedule/DemoSpringbootQuartzScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootquartzschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootQuartzScheduleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoSpringbootQuartzScheduleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/demospringbootquartzschedule/DemoSpringbootQuartzScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootquartzschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootQuartzScheduleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
