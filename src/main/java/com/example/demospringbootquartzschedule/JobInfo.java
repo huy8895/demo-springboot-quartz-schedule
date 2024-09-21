2afdb27c39bfb47db5072992518a4e552b9c5386
huytvdev <huy8895@gmail.com>
2024-09-21 15:15:08 +0700

    update: refactor dto

--- src/main/java/com/example/demospringbootquartzschedule/JobInfo.java
+++ src/main/java/com/example/demospringbootquartzschedule/JobInfo.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.Data;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
+import org.quartz.SimpleTrigger;
 import org.quartz.Trigger;
 import org.quartz.Trigger.TriggerState;
 
@@ -29,18 +30,49 @@ public class JobInfo {
         private String triggerName;
         private String triggerGroup;
         private Date startTime;
+        private Date endTime;
         private Date nextFireTime;
+        private Date previousFireTime;
+        private Date finalFireTime;
+        private boolean mayFireAgain;
         private TriggerState triggerState;
+        private String calendarName;
+        private String description;
+        private int misfireInstruction;
+        private int priority;
+        private long repeatInterval;
+        private int repeatCount;
+        private JobDataMap jobDataMap;
 
         public TriggerInfo(Trigger trigger, TriggerState triggerState) {
             this.triggerName = trigger.getKey().getName();
             this.triggerGroup = trigger.getKey().getGroup();
             this.startTime = trigger.getStartTime();
+            this.endTime = trigger.getEndTime();
             this.nextFireTime = trigger.getNextFireTime();
+            this.previousFireTime = trigger.getPreviousFireTime();
+            this.finalFireTime = trigger.getFinalFireTime();
+            this.mayFireAgain = trigger.mayFireAgain();
             this.triggerState = triggerState;
+            this.calendarName = trigger.getCalendarName();
+            this.description = trigger.getDescription();
+            this.misfireInstruction = trigger.getMisfireInstruction();
+            this.priority = trigger.getPriority();
+            
+            // Các thuộc tính sau có thể cần kiểm tra loại Trigger cụ thể
+            if (trigger instanceof SimpleTrigger) {
+                SimpleTrigger simpleTrigger = (SimpleTrigger) trigger;
+                this.repeatInterval = simpleTrigger.getRepeatInterval();
+                this.repeatCount = simpleTrigger.getRepeatCount();
+            } else {
+                this.repeatInterval = -1;
+                this.repeatCount = -1;
+            }
+            
+            this.jobDataMap = trigger.getJobDataMap();
         }
 
-        // Getters và Setters
+        // Getters và Setters (được tạo tự động bởi @Data của Lombok)
     }
 }
 
