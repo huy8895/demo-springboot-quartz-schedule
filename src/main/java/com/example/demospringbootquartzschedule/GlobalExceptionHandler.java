4a15e1a510bed993cff63636f5bb2e5c4aa73239
huytvdev <huy8895@gmail.com>
2024-09-22 11:06:18 +0700

    done: handle exception

---
+++ src/main/java/com/example/demospringbootquartzschedule/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.example.demospringbootquartzschedule;
+
+import com.example.demospringbootquartzschedule.dto.ErrorResponse;
+import org.quartz.ObjectAlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.quartz.SchedulerException;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ObjectAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleJobAlreadyExistsException(ObjectAlreadyExistsException ex, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.CONFLICT.value(),
+            HttpStatus.CONFLICT.getReasonPhrase(),
+            ex.getMessage(),
+            request.getDescription(false).substring(4)
+        );
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(SchedulerException.class)
+    public ResponseEntity<ErrorResponse> handleSchedulerException(SchedulerException ex, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+            ex.getMessage(),
+            request.getDescription(false).substring(4)
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+            "Đã xảy ra lỗi không mong muốn",
+            request.getDescription(false).substring(4)
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
